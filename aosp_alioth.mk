afcb24e4a2fd04eb12b42e555dcbf5a8d130ea36
andrej <andrejw39@gmail.com>
2022-02-26 01:46:08 +0100

    alioth: adapt to aex

---
+++ aosp_alioth.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2021 AospExtended
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common AospExtended stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+# Inherit from alioth device
+$(call inherit-product, device/xiaomi/alioth/device.mk)
+
+PRODUCT_NAME := aosp_alioth
+PRODUCT_DEVICE := alioth
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F3
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="alioth_global-user 11 RKQ1.200826.002 V12.5.4.0.RKHMIXM release-keys"
+
+BUILD_FINGERPRINT := Redmi/alioth_global/alioth:11/RKQ1.200826.002/V12.5.4.0.RKHMIXM:user/release-keys
